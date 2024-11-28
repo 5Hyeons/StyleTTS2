65fe2daea56a057a293e30ddc3f0445c25277e74
5Hyeons <ohs2251@naver.com>
2024-11-28 13:53:35 +0000

    feat: Enhance training logic to support step-based checkpointing and adjust sample rate

--- train_first.py
+++ train_first.py
@@ -153,10 +153,16 @@ def main(config_path):
         if config.get('pretrained_model', '') != '':
             model, optimizer, start_epoch, iters = load_checkpoint(model,  optimizer, config['pretrained_model'],
                                         load_only_params=config.get('load_only_params', True))
-            start_epoch += 1
+            if 'step' in config['pretrained_model']:
+                start_step = int(re.search('step_(.*?).pth', config['pretrained_model']).group(1)) + 2
+                print('Start step:', start_step)
+            else:
+                start_epoch += 1
+                start_step = 0
         else:
             start_epoch = 0
             iters = 0
+            start_step = 0
     
     # in case not distributed
     try:
@@ -178,8 +184,13 @@ def main(config_path):
         start_time = time.time()
 
         _ = [model[key].train() for key in model]
-
-        for i, batch in enumerate(train_dataloader):
+        if epoch > start_epoch:
+            start_step = 0
+        
+        for i, batch in enumerate(train_dataloader, start=start_step):
+            if i == len(train_dataloader):
+                print(f'Break at {i}')
+                break
             waves = batch[0]
             batch = [b.to(device) for b in batch[1:]]
             texts, input_lengths, _, _, mels, mel_input_length, _ = batch
